bc1fe0ae1fb19c02458347cb6346f25ef1da9089
bomhoe <nhk.nguyen1994@gmail.com>
2019-05-29 17:05:11 +0700

    a

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -16,6 +16,7 @@ class HomeState extends State<Home>{
   PopupHelper _popup;
   User _user;
   GlobalKey<ScaffoldState> _scaffoldKey;
+  GlobalKey<RefreshIndicatorState> _refreshKey;
   int _bottomIndex;
   int _starIndex;
 
@@ -27,25 +28,176 @@ class HomeState extends State<Home>{
     _popup = new PopupHelper(context);
     _user = new User(context);
     _scaffoldKey = new GlobalKey<ScaffoldState>();
+    _refreshKey = new GlobalKey<RefreshIndicatorState>();
     _bottomIndex = 0;
   }
 
-   StarBody() async {
+  Future<Null> _refreshState() async{
+    setState(() {
+      _bottomIndex = _bottomIndex;
+
+    });
+
+  }
+
+  StarBody()  {
     switch(_starIndex){
       case 0:
         Movie movie = new Movie();
-        List<Movie> data = await movie.GetMoviePlaying();
-        return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context,index){
-              return ListTile(title: Text(data[index].film_name),);
-            });
+        return FutureBuilder(
+          future:  movie.GetMoviePlaying(),
+          builder: (context,snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting)
+              return Center(child: CircularProgressIndicator(backgroundColor: Colors.orangeAccent,valueColor: AlwaysStoppedAnimation<Color>(Colors.white),),);
+            else{
+
+              if(snapshot.data != null){
+                List<Movie> data = snapshot.data;
+                return ListView.builder(
+                    itemCount: data.length>3?3:data.length,
+                    itemBuilder: (context,index){
+                      return Container(
+                          height: MediaQuery.of(context).size.width*1.1,
+                          width: MediaQuery.of(context).size.width*0.8,
+                          margin: EdgeInsets.all(7),
+                          child: Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            clipBehavior: Clip.antiAlias,
+                            borderOnForeground: true,
+                            elevation: 1,
+                            child:Stack(
+                              children: <Widget>[
+                                Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(image: NetworkImage(data[index].poster_url),fit: BoxFit.fill),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0.0,
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    height: MediaQuery.of(context).size.height*0.6,
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          begin: FractionalOffset.bottomCenter,
+                                          end: FractionalOffset.center,
+                                          colors: [
+                                            Color(0xff000000),
+                                            Colors.black.withOpacity(0.0)
+                                          ]
+                                      ),
+                                      //color: Colors.transparent
+                                    ),
+                                  ),
+                                ),// Linear gradient
+                                Positioned(
+                                  bottom:0,
+                                  left: 10,
+                                  child: Container(
+                                    alignment: FractionalOffset.topLeft,
+                                    width: MediaQuery.of(context).size.width*0.5,
+                                    height: MediaQuery.of(context).size.height*0.14,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Text("ĐANG CHIẾU",style: TextStyle(color: Colors.white,fontSize: 15,),),
+                                        ),
+                                        Align(
+                                          alignment: Alignment.topLeft,
+                                          child:  Text(data[index].film_name_vn,style: TextStyle(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 24),overflow: TextOverflow.clip,)
+
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),// Movie name
+                                Positioned(
+                                  bottom:0,
+                                  right: 10,
+                                  child: Container(
+                                    alignment: FractionalOffset.topRight,
+                                    width: MediaQuery.of(context).size.width*0.5,
+                                    height: MediaQuery.of(context).size.height*0.12,
+                                    child: Column(
+                                      children: <Widget>[
+                                        ButtonTheme(
+                                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                          minWidth: 60,
+                                          child: RaisedButton(
+                                            child: Text("ĐẶT VÉ",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w400),),
+                                            color: Colors.deepOrange,
+                                            onPressed: (){
+
+                                            },
+                                          ),
+                                        ),
+
+                                      ],
+                                    ),
+                                  ),
+                                ),// Book button
+                                Positioned(
+                                  top:10,
+                                  right: 10,
+                                  child: Container(
+                                    alignment: FractionalOffset.center,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.grey.withOpacity(0.5),
+
+                                    ),
+                                    width: MediaQuery.of(context).size.width*0.18,
+                                    height: MediaQuery.of(context).size.height*0.07,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(data[index].avg_point_showing.toString(),style: TextStyle(color: Colors.white,fontSize: 14,fontWeight: FontWeight.w400),),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Icon((data[index].avg_point_all >= 2?Icons.star:Icons.star_half),color: Colors.red,size: 12,),
+                                            Icon((data[index].avg_point_all >= 4?Icons.star:Icons.star_half),color: Colors.red,size: 12,),
+                                            Icon((data[index].avg_point_all >= 6?Icons.star:Icons.star_half),color: Colors.red,size: 12,),
+                                            Icon((data[index].avg_point_all >= 8?Icons.star:Icons.star_half),color: Colors.red,size: 12,),
+                                            Icon((data[index].avg_point_all >= 10?Icons.star:Icons.star_half),color: Colors.red,size: 12,),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),// Rating score
+                              ],
+                            ),
+                          )
+                      );
+                    });
+              }
+              else{
+                return Center(
+                  child: RaisedButton(
+                      child: Text("Tải lại"),
+                      onPressed: (){
+                        setState(() {
+
+                        });
+                      }
+                  ),
+                );
+              }
+
+            }
+
+          },
+        );
+
     }
   }
 
-   BodyData() async {
+  BodyData() {
     switch(_bottomIndex){
-      case 0:_starIndex = 0; return await StarBody();
+      case 0:_starIndex = 0; return StarBody();
     }
   }
 
@@ -108,65 +260,7 @@ class HomeState extends State<Home>{
               body: BodyData(),
             )
         )
-      /*Scaffold(
-        resizeToAvoidBottomInset: false,
-        key: _scaffoldKey,
-        appBar: AppBar(
-          *//*leading:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              InkWell(
-                child: Icon(Icons.menu),
-                highlightColor: Colors.deepPurpleAccent,
-                onTap: (){
-                  _scaffoldKey.currentState.openDrawer();
-                },
-              ),// Menu,
-              Spacer(),
-              InkWell(
-                child: Icon(Icons.assignment_ind),
-                highlightColor: Colors.deepPurpleAccent,
-                onTap: (){
-                  _user.EditPopup();
-                },
-              ),// User Information
-            ],
-          ),*//*
-          actions: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    width: MediaQuery.of(context).size.width*0.5,
-                    child:TextField(
-                      controller: _txtSearch,
-                      cursorColor: Colors.white,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Tìm kiếm",
-                        hintStyle: TextStyle(color: Colors.white),
-                        prefixIcon: Icon(Icons.search,color: Colors.white,),
 
-                      ),
-                      onSubmitted: (value){
-                        _popup.ShowPopup(message: value);
-                      },
-                    )
-                )
-              ],
-            ), // Search Field
-
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              ListTile(title: Text("a"),)
-            ],
-          ),
-        ),
-      ),*/
     );
   }
 }
--- lib/Model/Movie.dart
+++ lib/Model/Movie.dart
@@ -2,15 +2,15 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 class Movie{
-  final int avg_point;
-  final int avg_point_all;
-  final int avg_point_showing;
+  final double avg_point;
+  final double avg_point_all;
+  final double avg_point_showing;
   final String count_comment;
   final String count_like;
   final String count_share;
-  final int film_age;
-  final int film_duration;
-  final int film_id;
+  final double film_age;
+  final double film_duration;
+  final double film_id;
   final String film_language;
   final String film_name;
   final String film_name_en;
@@ -19,9 +19,9 @@ class Movie{
   final String film_slug_new;
   final String film_url;
   final String film_version;
-  final int has_session;
-  final int imdb_point;
-  final int is_new;
+  final double has_session;
+  final double imdb_point;
+  final double is_new;
   final String media_id;
   final String pg_rating;
   final String poster_landscape;
@@ -30,9 +30,9 @@ class Movie{
   final String poster_url;
   final String publish_date;
   final String showing_date;
-  final int status_id;
-  final int total_rating;
-  final int type_id;
+  final double status_id;
+  final double total_rating;
+  final double type_id;
 
   Movie({
     this.avg_point,this.avg_point_all,this.avg_point_showing,this.count_comment,this.count_like,this.count_share,this.film_age,this.film_duration,this.film_id,
@@ -41,17 +41,17 @@ class Movie{
     this.showing_date,this.status_id,this.total_rating,this.type_id
   });
 
-  factory Movie.parseJson(Map<String,dynamic> json){
-    return Movie(
-      avg_point: json["avg_point"],
-      avg_point_all: json["avg_point_all"],
-      avg_point_showing: json["avg_point_showing"],
+  factory Movie.parseJson(Map<String,dynamic> json)  {
+   return Movie(
+      avg_point: double.parse(json["avg_point"].toString()),
+      avg_point_all: double.parse(json["avg_point_all"].toString()),
+      avg_point_showing: double.parse(json["avg_point_showing"].toString()),
       count_comment: json["count_comment"],
       count_like: json["count_like"],
       count_share: json["count_share"],
-      film_age: json["film_age"],
-      film_duration: json["film_duration"],
-      film_id: json["film_id"],
+      film_age: double.parse(json["film_age"].toString()),
+      film_duration: double.parse(json["film_duration"].toString()),
+      film_id: double.parse(json["film_id"].toString()),
       film_language: json["film_language"],
       film_name: json["film_name"],
       film_name_en: json["film_name_en"],
@@ -60,9 +60,9 @@ class Movie{
       film_slug_new: json["film_slug_new"],
       film_url: json["film_url"],
       film_version: json["film_version"],
-      has_session: json["has_session"],
-      imdb_point: json["imdb_point"],
-      is_new: json["is_new"],
+      has_session: double.parse(json["has_session"].toString()),
+      imdb_point: double.parse(json["imdb_point"].toString()),
+      is_new: double.parse(json["is_new"].toString()),
       media_id: json["media_id"],
       pg_rating: json["pg_rating"],
       poster_landscape: json["poster_landscape"],
@@ -71,25 +71,26 @@ class Movie{
       poster_url: json["poster_url"],
       publish_date: json["publish_date"],
       showing_date: json["showing_date"],
-      status_id: json["status_id"],
-      total_rating: json["total_rating"],
-      type_id: json["type_id"],
+      status_id: double.parse(json["status_id"].toString()),
+      total_rating: double.parse(json["total_rating"].toString()),
+      type_id: double.parse(json["type_id"].toString()),
     );
   }
 
-  Future<List<Movie>> GetMoviePlaying() async {
+  Future<List<Movie>> GetMoviePlaying()  async {
 
-    var list = await http.post(
+    var result = await http.post(
         "https://123phim.vn/apitomapp",
         headers: {"Content-Type": "application/json"},
         body:'{"param":{"url":"/film/list?status=2","keyCache":"showing-film"},"method":"GET"}'
     ).then((http.Response response){
       if(response.statusCode == 200){
         Iterable originData = json.decode(response.body)["result"];
-        return originData.map((x)=> Movie.parseJson(x)).toList();
+
+        return originData.map((x)=>Movie.parseJson(x)).toList();
       }
     });
-    return list;
-  }
+    return result;
+}
 
 }
